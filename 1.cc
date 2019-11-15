33125b6469557d37982425df06ed5aba13b7eb52
Shubham Paliwal <shubhampaliwal1108@gmail.com>
2019-11-15 19:52:32 +0530

    added new file 1.cc

---
+++ 1.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<ii> vii;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define matb(n, m, flag) vector(n, vb(m, flag))
+#define mati(n, m, val) vector(n, vi(m, val))
+#define fr0(n) for(int i = 0; i < n; i++) 
+#define fr1(n) for(int i = 1; i <= n; i++) 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "hello world";
+}
